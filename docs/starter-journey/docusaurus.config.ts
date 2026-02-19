4616ac8f8ebe9468e8c7b7bc6bc5fb8af069735b
ivancalvo-dbxs <ivan.calvo@databricks.com>
2026-02-19 16:26:26 -0600

    new run

--- docs/starter-journey/docusaurus.config.ts
+++ docs/starter-journey/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
 
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  // For GitHub pages deployment, it is often '/<projectName>/'s
   baseUrl: '/',
   trailingSlash: true,
 
